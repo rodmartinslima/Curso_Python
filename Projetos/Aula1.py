02b36793ec8b17619cec775f640ec7445290aa99
Rodrigo Martins <rod.martinslima@gmail.com>
2023-10-31 20:30:02 -0300

    First Commit
    Criado repositório e commit da primeira aula.

---
+++ Projetos/Aula1.py
@@ -0,0 +1,13 @@
+#Iniciando a programação em Python
+print('Olá mundo python')
+valor = int(input("Informe um valor.."))
+
+if valor < 0:
+    print("O valor tem que ser maior do que zero!")
+
+if valor == 19:
+    print("Número da sorte =D ")
+elif valor == 22:
+    print("Número péssimo")
+else:
+    print("O valor digitado foi ", valor)
